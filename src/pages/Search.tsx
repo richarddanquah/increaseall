26d6f40b61a0c16b898946753f2ba3f254bee04b
Richard  Danquah <richpee60@yahoo.com>
2023-02-18 23:39:06 +0000

    new

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -3,6 +3,19 @@ import { BsChevronRight } from "react-icons/bs";
 import { BiPlus, BiBookmark, BiFilter } from "react-icons/bi";
 import { AiOutlineSearch } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 export default function Search() {
   return (
@@ -38,7 +51,7 @@ export default function Search() {
                 <input
                   type="text"
                   id="input-group-1"
-                  className="bg-gray-50 border border-gray-700 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-96 pl-7 p-2.5  dark:bg-white dark:border-white dark:placeholder-black dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-200 border border-gray-700 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-96 pl-7 p-2.5  dark:bg-white dark:border-gray-500 dark:placeholder-black dark:text-black dark:focus:ring-gray-500 dark:focus:border-gray-500"
                   placeholder="Search by Item #, Order #"
                 />
               </div>
@@ -68,6 +81,33 @@ export default function Search() {
         <br />
         <br />
         <br />
+        <Box sx={{ width: "100%" }}>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
+            <Grid item xs={12}>
+              <Item>
+              <span className="text-xl font-bold text-black">What are you looking for ?</span>
+              <br />
+              <br />
+              <span className="text-sm font-normal text-blue-300">Get started by searching & filtering a few</span>
+              <br />
+              <br />
+          
+              <Button variant="contained">Fetch data</Button>
+              <br />
+              <br />
+              <span className="text-sm font-normal text-gray-300">or</span>
+              <span className="text-sm font-normal text-blue-300">  search for an item</span>
+              </Item>
+            </Grid>
+        
+          
+          
+          </Grid>
+        </Box>
 
         {/* <div className="flex justify-between items -center px-14 h-14">
           <span className="text-sm font-medium text-gray-500">0 items</span>
